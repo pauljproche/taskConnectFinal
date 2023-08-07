6d0bf2ac515ed645e2d04cfed114069070a47c5e
pauljproche <paul.jp.roche@gmail.com>
2023-08-07 19:49:52 -0400

    Update display_BASIC.js

--- display_BASIC.js
+++ display_BASIC.js
@@ -144,7 +144,7 @@ http.createServer(async function (req, res) {
                         <h2>TaskConnect!</h2>
                         <a href="signuppage.html" class="button">Sign Up</a>
                     </div>
-                    <img src="http://localhost:3000/smile.png" alt="Image" class="right-image">
+                    <img src="Images/frontpage.png" alt="Image" class="right-image">
                 </div>
             </body>
             </html>`;
