a027f46f45c14c119285775cdcd043610eda4ad8
pauljproche <paul.jp.roche@gmail.com>
2023-08-08 19:55:38 -0400

    Update display_BASIC.js

--- display_BASIC.js
+++ display_BASIC.js
@@ -81,7 +81,7 @@ http.createServer(async function (req, res) {
                     document.getElementById('rightNav').innerHTML += "<br/>";
                     document.getElementById('rightNav').innerHTML += queryResult2Array[0].age;
                     document.getElementById('rightNav').innerHTML += "<br/>";
-                    document.getElementById('rightNav').innerHTML += queryResult2Array[0].follower[0];
+                    document.getElementById('rightNav').innerHTML += queryResult2Array[0].follower[0].name;
 
                   </script>
                   </html>`;
