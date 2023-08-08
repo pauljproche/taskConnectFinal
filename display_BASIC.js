5d9b0ff88c4e2b0269ebb4bf39afae9bd783f305
pauljproche <paul.jp.roche@gmail.com>
2023-08-08 10:10:53 -0400

    Update display_BASIC.js

--- display_BASIC.js
+++ display_BASIC.js
@@ -128,7 +128,7 @@ http.createServer(async function (req, res) {
                             <li><a href="index.html">
                                 <img class="icon" src="https://pauljproche.github.io/taskConnectFinal/Images/logo.png" alt="company logo" />
                             </a></li>
-                            <li class="hide-on-small-screen"><a href="index.html">TaskConnect</a></li>
+                            <li class="hide-on-small-screen"><a href="/home">TaskConnect</a></li>
                         </ul>
             
                         <ul>
