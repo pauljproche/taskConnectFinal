7dcf6206a00f60c7ed36e861fb53aacbca27b060
pauljproche <paul.jp.roche@gmail.com>
2023-08-08 23:00:55 -0400

    Update display_BASIC.js

--- display_BASIC.js
+++ display_BASIC.js
@@ -183,12 +183,12 @@ http.createServer(async function (req, res) {
                     
                     const queryResult1Array = ${queryResult1String};
 
-                    
+                    /*
                     document.getElementById('leftNav').innerHTML = "<h2> Task Analytics </h2><br/>";
                     document.getElementById('leftNav').innerHTML += <h3> Task 1 Progress: </h3><br/>";
                     document.getElementById('leftNav').innerHTML += queryResult1Array[0].taskName + "<br/>";
                     
-                    /*
+                    
                     document.getElementById('leftNav').innerHTML += queryResult1Array[0].dueDate + "<br/>";
                     //document.getElementById('leftNav').innerHTML += "Completion Status: " + queryResult1Array[0].isSubTask + "<br/>";
                     */
